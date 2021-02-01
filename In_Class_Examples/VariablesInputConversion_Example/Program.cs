b411f6a32e133218d5a373ee6631e7c4cde9fbbf
Adam Ackerman <adam@ou.edu>
2021-02-01 14:02:57 -0600

    Completed the program over input/output/variables/conversion/formatting

--- In_Class_Examples/VariablesInputConversion_Example/Program.cs
+++ In_Class_Examples/VariablesInputConversion_Example/Program.cs
@@ -6,7 +6,23 @@ namespace VariablesInputConversion_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int heresMyInt;
+            double heresMyDouble;
+
+            Console.WriteLine($"Please enter a double >>");
+            string answer = Console.ReadLine();
+            heresMyDouble = Convert.ToDouble(answer);
+
+            Console.WriteLine($"Please enter a integer >>");
+            answer = Console.ReadLine();
+            heresMyInt = Convert.ToInt32(answer);
+              
+            double result = heresMyDouble + heresMyInt;
+
+            //Console.WriteLine("The result of " + heresMyDouble.ToString("N3") + " + " +
+            //    heresMyInt.ToString() + " = " + result.ToString("C5"));
+            Console.WriteLine($"The result of {heresMyDouble.ToString("N3")} + {heresMyInt.ToString()} = {result.ToString("C5")}.");
+
         }
     }
 }
