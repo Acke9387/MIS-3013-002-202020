81a4b560274095c4125a7c25c71ccef2870cefde
Adam Ackerman <adam@ou.edu>
2021-02-08 13:43:46 -0600

    Started the second participation

---
+++ Participations/P2_Conditional-CoinToss/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P2_Conditional_CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1,3);
+            Console.WriteLine($"{randomNbr}");
+        }
+    }
+}
