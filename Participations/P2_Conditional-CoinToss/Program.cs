8d3953b7f609645b59c436a83763f53a79c8cf36
Ackerman, Adam P <adam@ou.edu>
2021-02-10 13:04:55 -0600

    Added a structure in comments for the participation

--- Participations/P2_Conditional-CoinToss/Program.cs
+++ Participations/P2_Conditional-CoinToss/Program.cs
@@ -7,8 +7,21 @@ namespace P2_Conditional_CoinToss
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int randomNbr = rand.Next(1,3);
+            int randomNbr = rand.Next(1,3); // generate a random number between 1 and 2
             Console.WriteLine($"{randomNbr}");
+
+            // 1 = heads
+            // 2 = tails
+
+            // Get the user's guess, 'Heads' or 'Tails'
+
+
+            // Convert their guess to an int or our int to a string 'Heads' or 'Tails' hint: conditionals
+
+
+            // Tell the user if they guessed correctly or not hint:conditionals
+
+
         }
     }
 }
