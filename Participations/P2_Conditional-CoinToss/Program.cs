4073feeba1cb3118ee589d8392c285513de6464b
Adam Ackerman <adam@ou.edu>
2021-02-22 13:10:36 -0600

    Completed the participation over the coin toss

--- Participations/P2_Conditional-CoinToss/Program.cs
+++ Participations/P2_Conditional-CoinToss/Program.cs
@@ -6,9 +6,50 @@ namespace P2_Conditional_CoinToss
     {
         static void Main(string[] args)
         {
+            const string DEVELOPER_NAME = "Instructor Ackerman";
             Random rand = new Random();
+            // Generate a random number between 1 and 2
             int randomNbr = rand.Next(1,3);
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine($"{randomNbr}");
+
+            const int HEADS = 1;
+            const int TAILS = 2;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Heads or Tails >>");
+            string answer = Console.ReadLine();
+
+            int answerAsInt;
+
+            if (answer.ToLower() == "heads")
+            {
+                answerAsInt = HEADS;
+            }
+            else
+            {
+                answerAsInt = TAILS;
+            }
+
+            if (randomNbr == HEADS)
+            {
+                Console.WriteLine("Coin flip came up HEADS");
+            }
+            else
+            {
+                Console.WriteLine("Coin flip came up TAILS");
+            }
+
+            if (answerAsInt == randomNbr)
+            {
+                
+                Console.WriteLine($"WOW good guess!");
+            }
+            else
+            {
+                Console.WriteLine($"Shucks, better luck next time.");
+            }
+
+            Console.WriteLine("\n\n" + "Created by : "+ DEVELOPER_NAME);
         }
     }
 }
