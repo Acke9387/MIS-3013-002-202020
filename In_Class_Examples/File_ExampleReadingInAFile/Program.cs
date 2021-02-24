3ba875b36bef4e03d500301b8e568146b7b3c9c4
Adam Ackerman <adam@ou.edu>
2021-02-24 14:04:16 -0600

    checked in code for example of for loops and reading a file

---
+++ In_Class_Examples/File_ExampleReadingInAFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace File_ExampleReadingInAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C:\Users\acke9387\Downloads\From the Land of the Snow-Pearls Tales from Puget Sound by Ella Higginson.txt
+
+            string[] lines = File.ReadAllLines("From the Land of the Snow-Pearls Tales from Puget Sound by Ella Higginson.txt");
+
+            string book = File.ReadAllText("From the Land of the Snow-Pearls Tales from Puget Sound by Ella Higginson.txt");
+
+            Console.WriteLine(book);
+
+        }
+    }
+}
