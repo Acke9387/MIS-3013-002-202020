35ea64074836003e38f27ea2a88fc366d49f1ad9
Adam Ackerman <adam@ou.edu>
2021-03-08 13:27:11 -0600

    Committing midterm example code

---
+++ Midterm_Review/Midterm_Review/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Midterm_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age >>");
+            string answer = Console.ReadLine();
+            int age;
+
+            if (int.TryParse(answer, out  age) == true)
+            {
+                Console.WriteLine("Good job entering a number");
+            }
+            else
+            {
+                Console.WriteLine("Bad job entering a number");
+                Environment.Exit(-1);
+            }
+
+        }
+    }
+}
---
+++ Midterm_Review/Substring_Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Substring_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string statement = "The quick brown fox jumped over the lazy dog";
+            int locationOfFirstSpace = statement.IndexOf(" ");
+            string firstWord = statement.Substring(0, locationOfFirstSpace);
+
+            Console.WriteLine(firstWord);
+
+        }
+    }
+}
