43f8486a6c2136cf93592c9593cba3c4eab40b4c
Adam Ackerman <adam@ou.edu>
2021-04-14 13:09:56 -0500

    finished the speak participation

---
+++ Participations/Functions_Speak/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Functions_Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What animal would you like to hear the noise of? >>");
+            string answer = Console.ReadLine();
+            string animalNoise = Speak(answer);
+            //Console.WriteLine($"{answer} makes the sound {Speak(answer)}");
+            Console.WriteLine($"{answer} makes the sound {animalNoise}");
+
+        }
+
+        static string Speak(string animal)
+        {
+            string sound;
+
+            if (animal.ToLower() == "dog")
+            {
+                sound = "woof";
+            }
+            else if (animal.ToLower() == "cat")
+            {
+                sound = "purr";
+            }
+            else if (animal.ToLower() == "goat")
+            {
+                sound = "person yelling";
+            }
+            else
+            {
+                sound = "Grmbl";
+            }
+
+            return sound;
+        }
+    }
+}
