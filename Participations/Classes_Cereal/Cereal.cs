4f0a31086759915c8f07dcfebb9d8a2e9c06d781
Adam Ackerman <adam@ou.edu>
2021-05-05 12:55:21 -0500

    finished the Cereal participation

---
+++ Participations/Classes_Cereal/Cereal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Cereal
+{
+    public class Cereal
+    {
+
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = string.Empty;
+            Name = "";
+            Calories = 0;
+            Cups = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} made by {Manufacturer} has a serving size of {Cups.ToString("N1")} and has {Calories.ToString("N2")} calories.";
+        }
+
+
+    }
+}
--- Participations/Classes_Cereal/Program.cs
+++ Participations/Classes_Cereal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,9 +9,58 @@ namespace Classes_Cereal
     {
         static void Main(string[] args)
         {
-
             string[] lines = File.ReadAllLines("Cereal_Data.txt");
+            //              0       1           2     3
+            //lines[0] = "name|manufacturer|calories|cups";
+
+            List<Cereal> bowls = new List<Cereal>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                // line = "100% Bran|Nabisco|70|0.33"
+
+
+                string[] cerealInformation = line.Split('|');
+                //                          0           1        2      3
+                //cerealInformation = {"100% Bran", "Nabisco", "70", "0.33"}
+                string name = cerealInformation[0];
+
+                Cereal c = new Cereal();
+                c.Name = name;
+                c.Manufacturer = cerealInformation[1];
+                c.Calories = Convert.ToDouble(cerealInformation[2]);
+                c.Cups = Convert.ToDouble(cerealInformation[3]);
+
+                bowls.Add(c);
+
+                //Console.WriteLine(c);
+            }
+
+            Console.WriteLine("\nAll cereals that have 1 cup or more for the serving size");
+            foreach (Cereal cereal in bowls)
+            {
+                if (cereal.Cups >= 1)
+                {
+                    Console.WriteLine(cereal);
+                }
+
+
+            }
+
+
+
+            Console.WriteLine("\nAll cereals that have 100 calories or fewer per serving");
+            foreach (Cereal cereal in bowls)
+            {
+                if (cereal.Calories <= 100)
+                {
+                    Console.WriteLine(cereal);
+                }
+            }
 
         }
+
+
     }
 }
